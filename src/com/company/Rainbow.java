5acd1831a4a9073b7f848958a2baac6daaeea4ea
Mary Kazaryan <82213157+mary-kazaryan@users.noreply.github.com>
2021-04-14 01:51:20 +0300

    Add files via upload

---
+++ src/com/company/Rainbow.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Rainbow {
+
+    public static void main(String[] args) {
+
+Scanner in = new Scanner (System.in);
+int colour = in.nextInt();
+
+        switch (colour) {
+            case 1:
+                System.out.println("Красный");
+                break;
+            case 2:
+                System.out.println("Оранжевый");
+                break;
+            case 3:
+                System.out.println("Желтый");
+                break;
+            case 4:
+                System.out.println("Зеленый");
+                break;
+            case 5:
+                System.out.println("Голубой");
+                break;
+            case 6:
+                System.out.println("Синий");
+                break;
+            case 7:
+                System.out.println("Фиолетовый");
+                break;
+            default:
+                System.out.println("Такого цвета радуги не существует");
+        }
+
+
+    }
+
+}
+
+
